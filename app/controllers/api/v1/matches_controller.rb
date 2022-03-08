cb119e1cfe737fed27319ee39c4ecb72c7ae3fe1
Jordan Wanlass <jwanlass8@gmail.com>
2022-03-08 16:02:20 -0700

    get 10 most recent matches

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -5,16 +5,18 @@ module Api
     # Controller for all match related api calls
     class MatchesController < ApplicationController
       def index
-        #   temp = {
-        #     "Accept-Language" => "en-US,en;q=0.9",
-        #     "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8",
-        #     "X-Riot-Token" => "RGAPI-02358cb1-1191-4de6-b3e4-85622a4de2ad",
-        #     {params:{start: 0, count: 20}}
-        # }
-        response = RestClient.get('https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/l8jGR7HcG5vMNsmmdQE4Mwgv3szls11ACmJuirQFQ9RZDiDeBmNOQCNpHBQhZ8BgBUzWbcOGe4s_Ug/ids',
-                                  { "Accept-Language" => "en-US,en;q=0.9", "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8",
-                                    "X-Riot-Token" => "RGAPI-02358cb1-1191-4de6-b3e4-85622a4de2ad", params: { 'start': 0, 'count': 20 } })
-        render json: response.body, status: 200
+        all_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/l8jGR7HcG5vMNsmmdQE4Mwgv3szls11ACmJuirQFQ9RZDiDeBmNOQCNpHBQhZ8BgBUzWbcOGe4s_Ug/ids"
+        single_url = "https://americas.api.riotgames.com/lol/match/v5/matches/"
+        header = { "Accept-Language" => "en-US,en;q=0.9", "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8",
+                   "X-Riot-Token" => "RGAPI-02358cb1-1191-4de6-b3e4-85622a4de2ad", params: { 'start': 0, 'count': 10 } }
+
+        response = RestClient.get(all_url, header)
+
+        matches = []
+        JSON.parse(response.body).each do |id|
+          matches.push(JSON.parse(RestClient.get(single_url + id, header.except(:params))))
+        end
+        render json: matches, status: 200
       end
 
       def show; end
