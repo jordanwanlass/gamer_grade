330226e3424addc123a6a67f1f019d6d4cbb30eb
Jordan Wanlass <jwanlass8@gmail.com>
2022-03-08 16:05:27 -0700

    remove key from code

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -8,7 +8,7 @@ module Api
         all_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/l8jGR7HcG5vMNsmmdQE4Mwgv3szls11ACmJuirQFQ9RZDiDeBmNOQCNpHBQhZ8BgBUzWbcOGe4s_Ug/ids"
         single_url = "https://americas.api.riotgames.com/lol/match/v5/matches/"
         header = { "Accept-Language" => "en-US,en;q=0.9", "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8",
-                   "X-Riot-Token" => "RGAPI-02358cb1-1191-4de6-b3e4-85622a4de2ad", params: { 'start': 0, 'count': 10 } }
+                   "", params: { 'start': 0, 'count': 10 } }
 
         response = RestClient.get(all_url, header)
 
