07361929e388bd200d83b8c8a9805f7057ae3aa7
Jordan Wanlass <jwanlass8@gmail.com>
2022-03-08 17:43:32 -0700

    pull minimal set of stats for the last 10 games

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -5,21 +5,32 @@ module Api
     # Controller for all match related api calls
     class MatchesController < ApplicationController
       def index
+        @puuid = 'l8jGR7HcG5vMNsmmdQE4Mwgv3szls11ACmJuirQFQ9RZDiDeBmNOQCNpHBQhZ8BgBUzWbcOGe4s_Ug'
+        @header = { "Accept-Language" => "en-US,en;q=0.9", "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8",
+                    , params: { 'start': 0, 'count': 10 } }
+
         all_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/l8jGR7HcG5vMNsmmdQE4Mwgv3szls11ACmJuirQFQ9RZDiDeBmNOQCNpHBQhZ8BgBUzWbcOGe4s_Ug/ids"
-        single_url = "https://americas.api.riotgames.com/lol/match/v5/matches/"
-        header = { "Accept-Language" => "en-US,en;q=0.9", "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8",
-                   "", params: { 'start': 0, 'count': 10 } }
-
-        response = RestClient.get(all_url, header)
-
-        matches = []
-        JSON.parse(response.body).each do |id|
-          matches.push(JSON.parse(RestClient.get(single_url + id, header.except(:params))))
-        end
-        render json: matches, status: 200
+        render json: build_small_matches(RestClient.get(all_url, @header)), status: 200
       end
 
       def show; end
+
+      def build_small_matches(response)
+        matches = []
+        JSON.parse(response.body).each do |id|
+          matches.push(get_small_match(JSON.parse(RestClient.get("https://americas.api.riotgames.com/lol/match/v5/matches/#{id}", @header.except(:params)))))
+        end
+        matches
+      end
+
+      def get_small_match(big_match)
+        { championName: big_match["info"]["participants"].find { |p| p["puuid"] == @puuid }["championName"],
+          kills: big_match["info"]["participants"].find { |p| p["puuid"] == @puuid }["kills"],
+          deaths: big_match["info"]["participants"].find { |p| p["puuid"] == @puuid }["deaths"],
+          assists: big_match["info"]["participants"].find { |p| p["puuid"] == @puuid }["assists"],
+          kda: big_match["info"]["participants"].find { |p| p["puuid"] == @puuid }["challenges"]["kda"]
+        }
+      end
     end
   end
 end
