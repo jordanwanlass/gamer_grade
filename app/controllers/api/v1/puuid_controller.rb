3766d0bced2551ac96e22f19af2e5a22242b9c8d
Jordan Wanlass <jwanlass8@gmail.com>
2022-03-10 15:26:36 -0700

    fix bug

--- app/controllers/api/v1/puuid_controller.rb
+++ app/controllers/api/v1/puuid_controller.rb
@@ -3,7 +3,7 @@ module Api
     # api for puuid retrieval
     class PuuidController < ApplicationController
       def index
-        header = { "Accept-Language" => "en-US,en;q=0.9", "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8", "X-Riot-Token" => "X-Riot-Token" => ENV["API_KEY"] }
+        header = { "Accept-Language" => "en-US,en;q=0.9", "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8", "X-Riot-Token" => ENV["API_KEY"] }
         render json: RestClient.get("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/#{params[:riotId]}/#{params[:tagline]}", header), status: 200
       end
     end
